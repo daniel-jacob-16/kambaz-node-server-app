61d38d74a3ea860e617614d934689984ba40bd7b
daniel-jacob-16 <dankjacob4@gmail.com>
2025-06-08 21:53:43 -0400

    some more done

--- Kambaz/Users/routes.js
+++ Kambaz/Users/routes.js
@@ -1,18 +1,35 @@
 import * as dao from "./dao.js";
-let currentUser = null;
+import * as courseDao from "../Courses/dao.js";
+
 export default function UserRoutes(app) {
   const createUser = (req, res) => { };
   const deleteUser = (req, res) => { };
   const findAllUsers = (req, res) => { };
   const findUserById = (req, res) => { };
+  
+  const findCoursesForEnrolledUser = (req, res) => {
+    let { userId } = req.params;
+    if (userId === "current") {
+      const currentUser = req.session["currentUser"];
+      if (!currentUser) {
+        res.sendStatus(401);
+        return;
+      }
+      userId = currentUser._id;
+    }
+    const courses = courseDao.findCoursesForEnrolledUser(userId);
+    res.json(courses);
+  };
+
   const updateUser = (req, res) => {
     const userId = req.params.userId;
     const userUpdates = req.body;
     dao.updateUser(userId, userUpdates);
-    currentUser = dao.findUserById(userId);
+    const currentUser = dao.findUserById(userId);
+    req.session["currentUser"] = currentUser;
     res.json(currentUser);
+  };
 
-   };
   const signup = (req, res) => {
     const user = dao.findUserByUsername(req.body.username);
     if (user) {
@@ -20,22 +37,35 @@ export default function UserRoutes(app) {
         { message: "Username already in use" });
       return;
     }
-    currentUser = dao.createUser(req.body);
-    res.json(currentUser);
-   };
-  const signin = (req, res) => { 
-    const { username, password } = req.body;
-    currentUser = dao.findUserByCredentials(username, password);
-    res.json(currentUser);
-  };
-    const profile = async (req, res) => {
+    const currentUser = dao.createUser(req.body);
+    req.session["currentUser"] = currentUser;
     res.json(currentUser);
   };
 
+  const signin = (req, res) => { 
+    const currentUser = dao.findUserByCredentials(username, password);
+    if (currentUser) {
+      req.session["currentUser"] = currentUser;
+    res.json(currentUser);
+    } else {
+      res.status(401).json({ message: "Unable to login. Try again later." });
+    }
+  };
+
+  const profile = async (req, res) => {
+      const currentUser = req.session["currentUser"];
+      if (!currentUser) {
+        res.sendStatus(401);
+        return;
+      }
+      res.json(currentUser);
+  };
+
   const signout = (req, res) => { 
-    currentUser = null;
+    req.session.destroy();
     res.sendStatus(200);
   };
+
   app.post("/api/users", createUser);
   app.get("/api/users", findAllUsers);
   app.get("/api/users/:userId", findUserById);
@@ -45,4 +75,5 @@ export default function UserRoutes(app) {
   app.post("/api/users/signin", signin);
   app.post("/api/users/signout", signout);
   app.post("/api/users/profile", profile);
+  app.get("/api/users/:userId/courses", findCoursesForEnrolledUser);
 }
--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import Lab5 from "./Lab5/index.js";
 import cors from "cors";
 import UserRoutes from "./Kambaz/Users/routes.js";
 import session from "express-session";
+import "dotenv/config";
+import CourseRoutes from "./Kambaz/Courses/routes.js";
 
 const app = express()
 app.use(cors({
@@ -11,16 +13,23 @@ app.use(cors({
    origin: process.env.NETLIFY_URL || "http://localhost:5173",
  })
 );
-app.use(express.json());
 const sessionOptions = {
-  secret: "any string",
+  secret: process.env.SESSION_SECRET || "kambaz",
   resave: false,
   saveUninitialized: false,
 };
-app.use(
-  session(sessionOptions)
-);
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.NODE_SERVER_DOMAIN,
+  };
+}
+app.use(session(sessionOptions));
+app.use(express.json());
 UserRoutes(app);
+CourseRoutes(app);
 Lab5(app);
-Hello(app)
-app.listen(process.env.PORT || 4000) 
+Hello(app);
+app.listen(process.env.PORT || 4000);
