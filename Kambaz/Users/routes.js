600edf8152a9f551fc83a160b781673643eb9c7b
daniel-jacob-16 <dankjacob4@gmail.com>
2025-06-17 18:32:06 -0400

    more done

--- Kambaz/Users/routes.js
+++ Kambaz/Users/routes.js
@@ -48,14 +48,17 @@ export default function UserRoutes(app) {
     res.json(courses);
   };
 
-  const updateUser = (req, res) => {
-    const userId = req.params.userId;
+  const updateUser = async (req, res) => {
+    const { userId } = req.params;
     const userUpdates = req.body;
-    dao.updateUser(userId, userUpdates);
-    const currentUser = dao.findUserById(userId);
-    req.session["currentUser"] = currentUser;
+    await dao.updateUser(userId, userUpdates);
+    const currentUser = req.session["currentUser"];
+   if (currentUser && currentUser._id === userId) {
+     req.session["currentUser"] = { ...currentUser, ...userUpdates };
+   }
     res.json(currentUser);
   };
+
 
   const signup = async (req, res) => {
     const user = await dao.findUserByUsername(req.body.username);
